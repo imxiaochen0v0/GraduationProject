56ee387e5cfeff16fbce02b4ce5cf3bbdd2990e3
imxiaochen0v0 <im-xiaochen@outlook.com>
2024-06-21 02:12:51 +0800

    更新 hotelList.vue

--- Roome/src/pages/index/hotelList.vue
+++ Roome/src/pages/index/hotelList.vue
@@ -30,7 +30,7 @@ const to = async (e) => {
         <view class="hotelList">
             <u-navbar :title="city.name" :autoBack="true" safeAreaInsetTop placeholder :fixed="false"></u-navbar>
 
-            <u-empty mode="favor" text="该城市暂无酒店" v-if="hotelList === undefined"></u-empty>
+            <u-empty mode="favor" text="暂无搜索结果" v-if="hotelList === undefined"></u-empty>
             <uni-card @click="to(item)" padding="0" margin="10" v-for="(item, index) in hotelList" :key="item.id">
                 <image :src="baseUrl + item.img" mode="widthFix" />
                 <view class="hotelContent">
